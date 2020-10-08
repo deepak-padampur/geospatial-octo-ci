140c938e60dcac4c32f791ce584816eda28ad63e
deepak-padampur <deepaksuna4290@gmail.com>
2020-10-09 03:19:55 +0530

    server logs added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 /* eslint-disable */
 const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const app = express();
+
+app.use(morgan('common'));
+app.use(helmet())
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Express Server"
+  })
+})
+
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => {

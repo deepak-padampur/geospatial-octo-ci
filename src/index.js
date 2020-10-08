61547c7c6812a89038715f6e9c6f2823d747a565
deepak-padampur <deepaksuna4290@gmail.com>
2020-10-09 02:41:36 +0530

    set up a basic server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const express = require('express')
+const app = express();
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
+})
+
+
+
